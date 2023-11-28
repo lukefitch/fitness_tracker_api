c15293a442272b85eb8e89ad383fee123b5a2613
Luke Fitch <luke.code99@gmail.com>
2023-11-28 12:03:06 -0700

    Your commit message

--- routes.py
+++ routes.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from models import db, User, Activity, DietLog, WorkoutPlan
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/fitness_tracker_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db:5432/fitness_tracker_db'
+app.config['SECRET_KEY'] = 'your-secret-key'  # replace with your actual secret key
 db.init_app(app)
 
 @app.route('/users', methods=['POST'])
@@ -10,12 +12,41 @@ def create_user():
     data = request.get_json()
     username = data['username']
     email = data['email']
+    password = data['password']
     user = User(username=username, email=email)
+    user.set_password(password)
     db.session.add(user)
     db.session.commit()
     return jsonify({'message': 'User created'}), 201
 
-# Add your other routes here
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data['username']
+    email = data['email']
+    password = data['password']
+    user = User(username=username, email=email)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'User registered'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        session['username'] = username
+        return jsonify({'message': 'Logged in'}), 200
+    else:
+        return jsonify({'message': 'Invalid username or password'}), 401
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('username', None)
+    return jsonify({'message': 'Logged out'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
